29bc81f7df58abfed11257b7fe665e89cc75d982
Jongho Hong <myodan@pm.me>
2024-11-13 21:00:27 +0900

    fix: 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,17 @@
-import {
-	Code,
-	Container,
-	For,
-	Grid,
-	HStack,
-	Stack,
-	VStack,
-} from "@chakra-ui/react";
+import { Container, For, Grid, HStack, Stack, VStack } from "@chakra-ui/react";
 import { ArrowRight } from "lucide-react";
 import NextLink from "next/link";
 import type { FC } from "react";
 import { Button } from "~/components/ui/button";
-import { getProfile, getToken } from "~/libs/api/auth";
 import { getWebtoons } from "~/libs/api/webtoons";
 import { WebtoonListItem } from "../components/webtoons/item";
 
 const HomePage: FC = async () => {
-	const token = await getToken();
-	const user = await getProfile();
 	const webtoons = await getWebtoons();
 
 	return (
 		<Container>
 			<Stack py="4" gap="4">
-				<Code as="pre" padding="4">
-					{JSON.stringify(user, null, 2)}
-				</Code>
-				<Code as="pre" padding="4">
-					{JSON.stringify(token, null, 2)}
-				</Code>
 				<VStack alignItems="stretch">
 					<HStack justify="space-between">
 						<NextLink
